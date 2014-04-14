48826019843fe6cfeb5aed8e7c118f69954ffdc6
Ozzie Gooen <Ozzie@BowLabs.org>
2014-04-14 14:13:27 -0400

    updated leap to pass tests

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -9,16 +9,20 @@ class Year
     @year = year
   end
 
-  def leap?(year)
-    year.div_by(400) or (!year.div_by(100) and year.div_by(4))
+  def leap?
+    div_by(400) or (!div_by(100) and div_by(4))
   end
 
   def self.leap?(year)
     year = Year.new(year)
+    year.leap?
   end
+
+  private 
 
   def div_by(i)
     @year%i == 0
   end
 
 end
+
