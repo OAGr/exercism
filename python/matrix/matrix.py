1975766e88451618520d1cc83c73ff72fbcf7bbb
Ozzie Gooen <Ozzie@BowLabs.org>
2014-04-15 18:50:41 -0400

    simplified matrix code

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,8 @@
 class Matrix(object):
     def __init__(self,text):
-        self.rows, self.columns = self.generate_matrix(text)
-
-    def generate_matrix(self, text):
-        rows = [[int(i) for i in row.split()] for row in text.split("\n")]
-        columns = [[row[i] for row in rows] for i in range(len(rows[0]))]
-        return rows, columns
-
+        self.rows = [[int(char) for char in line.split()]
+                                for line in text.split("\n")]
+        self.columns = [[row[i] for row in self.rows]
+                                for i in range(len(self.rows[0]))]
     
 
